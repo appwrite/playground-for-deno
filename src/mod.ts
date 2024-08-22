f4c1e6ba45774f394c98e499c1cfa58c836d7dc8
loks0n <22452787+loks0n@users.noreply.github.com>
2024-08-22 11:23:59 +0100

    fix: client

--- src/mod.ts
+++ src/mod.ts
@@ -3,11 +3,9 @@ import { Databases, User, Document, Storage, Function } from "./api/index.ts";
 
 // Configurations
 const client = new Client();
-client.setEndpoint("https://v16.appwrite.org/v1");
-client.setKey(
-  "standard_e7b66c0c36e4dc4c50190aa9dda77b09a8c94bc22e84ce12304da1c5be9bc9ba0046648031780242b6e19614297ab201785f0df28ece5953bd5d608ca35e13fd2688e200350ed0598de05249546683cc8187de6a7e2c00eb2ceb8973c5b8d23c476fc988b1b882bbd64ef72031da5c8fb53a87f89c46c266d26f1eb0bf795a39"
-);
-client.setProject("66c7080900194ba26959");
+client.setEndpoint("http://YOUR_HOST/v1");
+client.setKey("YOUR_API_KEY");
+client.setProject("YOUR_PROJECT_ID");
 // client.setJWT('jwt') // Use this to authenticate with JWT generated from client
 
 // API Calls
